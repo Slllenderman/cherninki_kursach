dcaa4ef7c4512bcacbf1e901d99036e18ca67b29
Maksim Khatin <mskhatin@avito.ru>
2023-12-26 00:38:11 +0300

    гипотезы

--- settings.py
+++ settings.py
@@ -35,5 +35,8 @@ MODELLING_TIME = 43200
 # Параметры отрисовки статистики~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 DRAW_EXPERIMENT = False
 DRAW_TEXT_STAT = True if not DRAW_EXPERIMENT else False
-DRAW_DYNAMIC_GRAPH = False if not DRAW_EXPERIMENT else False
+DRAW_DYNAMIC_GRAPH = False if not DRAW_EXPERIMENT else True
 PLOT_RATE = 100
+
+
+ALPHA = 0.05
